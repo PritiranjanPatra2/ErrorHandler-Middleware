3b2e832eb96a9a43fc955a16f62fe47c4cd2ee82
Pritiranjan <patrapritiranjan11@gmail.com>
2024-08-31 13:20:09 +0530

    error handling middeware

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { errorhandler, validate } from './ErrorHandling.js';
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.post('/user',validate,(req,res)=>{
+    console.log(req.body); 
+    console.log("user created");
+    
+    res.send('user created');
+
+    // console.log("user created");
+     
+})
+
+
+app.listen(9300,()=>{
+    console.log('Server is running on port 9300');
+})
+app.use(errorhandler)
